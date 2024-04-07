36f0be362a29ab214b30559579fb3a272f5b380f
foruseonly <147395222+foruseonly@users.noreply.github.com>
2024-04-07 23:07:45 +0530

    Add files via upload

---
+++ CC JOURNAL CODES.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#prac2:foss cloud steps#
+
+
+# In[ ]:
+
+
+prac 2-FOSS
+
+create vm
+192.168.10.3->login page
+username login
+password login
+
+open winscp 
+hostname(192.168.10.3)
+username root
+password admin
+login
+
+ubuntu iso file 
+drag and paste to var-virtualization-iso choosable
+()
+
+vm->create template
+profile-->create linux1 name and fill details and click create
+click green arrow 
+then click cd 
+
+
+# In[ ]:
+
+
+#prac3:saas#
+
+
+# In[ ]:
+
+
+prac 3-Saas
+administration->system and domain
+username administrator
+password admin@123
+login
+
+users management of domain
+add new user
+insert username and other details
+next
+set password->create user
+logout
+
+application->owncloud
+password in user management
+-----------------------------------------------
+Go to the OwnCloud IP address
+Click on System and Domain Settings
+Login using Administrator & admin@123 as username & password
+Click on Create New Users & Add new Users
+Then Logout 
+Click on OwnCloud application
+And Login
+using the Created User Credentials
+
